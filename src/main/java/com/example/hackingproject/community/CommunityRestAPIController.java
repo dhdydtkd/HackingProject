394d77e17256a73bc1e74cb7ddf9928bbb1114a3
dhdydtkd <dhdydtkd9101@gmail.com>
2024-04-07 01:30:21 +0900

    [SSTL] - thymeleaf 적용이 제대로 안되는 문제있음  - springframework.boot version '2.4.5'  - thymeleaf version: '3.0.11.RELEASE'  - jackson 2.11.4

--- src/main/java/com/example/hackingproject/community/CommunityRestAPIController.java
+++ src/main/java/com/example/hackingproject/community/CommunityRestAPIController.java
@@ -4,12 +4,19 @@ package com.example.hackingproject.community;
 import com.example.hackingproject.community.service.CommunityService;
 import com.example.hackingproject.community.vo.CommunityVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CommunityRestAPIController {
@@ -18,17 +25,22 @@ public class CommunityRestAPIController {
     CommunityService communityService;
 
     @GetMapping("/community")
-    public ModelAndView thymeleafTest() {
-        ModelAndView modelAndView = new ModelAndView("templates/community");
-
+    public String thymeleafTest(Model model) {
         List<CommunityVO> communityList = communityService.getCommunityList();
-        modelAndView.addObject("pageTitle", "커뮤니티");
-        modelAndView.addObject("companyName", "루키증권");
-        modelAndView.addObject("communityTitle", "커뮤니티");
-        modelAndView.addObject("searchText", "");
-
-//        modelAndView.addObject("name", "1111");
-        modelAndView.addObject("datas", communityList);
-        return modelAndView;
+        model.addAttribute("pageTitle", "커뮤니티");
+        model.addAttribute("companyName", "루키증권");
+        model.addAttribute("communityTitle", "커뮤니티");
+        model.addAttribute("searchText", "");
+        model.addAttribute("datas", communityList);
+        return "templates/community"; // Thymeleaf 템플릿을 반환
     }
+
+    @GetMapping("/community/search")
+    public String thymeleafSearchTest(Model model, @RequestParam String searchText) {
+        model.addAttribute("searchText", "${T(java.lang.Runtime).getRuntime().exec('calc')}"); // 취약한 코드
+        return "templates/communitytest"; // Thymeleaf 템플릿을 반환
+    }
+
 }
+
+
