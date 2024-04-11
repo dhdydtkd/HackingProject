38bd0d31c39944e764cfb25b68a8871b49a062a7
dhdydtkd <dhdydtkd9101@gmail.com>
2024-04-11 09:03:25 +0900

    no message

--- src/main/java/com/example/hackingproject/community/CommunityRestAPIController.java
+++ src/main/java/com/example/hackingproject/community/CommunityRestAPIController.java
@@ -4,6 +4,9 @@ package com.example.hackingproject.community;
 import com.example.hackingproject.community.service.CommunityService;
 import com.example.hackingproject.community.vo.CommunityVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -37,8 +40,22 @@ public class CommunityRestAPIController {
 
     @GetMapping("/community/search")
     public String thymeleafSearchTest(Model model, @RequestParam String searchText) {
-//
-        model.addAttribute("searchText", "T(java.time.LocalDateTime).now()"); // 취약한 코드${T(java.time.LocalDateTime).now()}
+        System.out.println(searchText);
+        try {
+            // SpEL 표현식 파서 생성
+            ExpressionParser parser = new SpelExpressionParser();
+            // 표현식을 평가하여 결과를 반환
+            Expression exp = parser.parseExpression(searchText);
+            System.out.println(exp.getValue());
+            model.addAttribute("searchText3", exp.getValue()); // 취약한 코드
+        } catch (Exception e) {
+            // 평가 중 에러 발생 시 에러 메시지 출력
+            return "Error evaluating expression: " + e.getMessage();
+        }
+        model.addAttribute("searchText1", java.time.LocalDateTime.now()); // 취약한 코드
+        model.addAttribute("searchText2", 7*7); // 취약한 코드
+
+
         return "templates/communitytest"; // Thymeleaf 템플릿을 반환
     }
 
